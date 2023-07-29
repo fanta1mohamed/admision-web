a48ecb4899d9334e3acccf96595ed0a3e9627234
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2023-07-28 22:43:57 -0500

    -

--- resources/views/ingreso/datosbiometricos.blade.php
+++ resources/views/ingreso/datosbiometricos.blade.php
@@ -91,7 +91,11 @@
                     </td>
                     <td style="">
                         <div style="width: 118px; height:150px;">
-                            <img src="{{ public_path('hexamencepre/'.$data.'x.jpg')}} " alt="" width="118">
+                            @if ($hexaI)
+                            <img src="{{ $hexaD }}" alt="Mi imagen" width="118">
+                            @else
+                                <p>La imagen no está disponible.</p>
+                            @endif
                             <div style="margin-top: -147px; heigth:10px; padding: 0px 10px;">
                                 <div style="background: #d9d9d900; text-align:center; width:100%;" >
                                     <div style="">
