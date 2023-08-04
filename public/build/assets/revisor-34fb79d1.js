eab8920f72ba30fb2e2f4bb6809e43a9809ba8b9
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2023-08-04 06:40:46 -0500

    -

---
+++ public/build/assets/revisor-34fb79d1.js
@@ -0,0 +1,1 @@
+import{k as t,w as e,o,d as r}from"./app-9f16b92a.js";import{A as a}from"./LayoutDocente-13779012.js";import"./DropdownLink-829ed0f0.js";import"./logotiny-c4b525af.js";import"./_plugin-vue_export-helper-c27b6911.js";const s=r("h1",null," Revisores ",-1),d={__name:"revisor",setup(c){return(i,_)=>(o(),t(a,null,{default:e(()=>[s]),_:1}))}};export{d as default};
