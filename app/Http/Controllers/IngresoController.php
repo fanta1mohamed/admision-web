8911e0ccb55749f07256f1f59c48dea605a878c5
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2023-08-29 04:58:23 -0500

    -

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -336,9 +336,7 @@ class IngresoController extends Controller
         $host = '10.1.1.134'; // Reemplaza con el host desde el que se conecta el usuario
 
         $query = "SHOW GRANTS FOR '$user'@'$host'";
-        $expression = DB::raw($query);
-        
-        $grants = DB::select(DB::raw($expression), $expression->getBindings());
+        $grants = DB::connection()->select($query);
         
         foreach ($grants as $grant) {
             echo $grant->Grants_for_user . "\n";
