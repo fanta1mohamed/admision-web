3f640a56498ff6073c090460d43a44f519876fda
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2023-09-15 09:24:26 -0500

    .

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         if( auth()->user()->id_rol == 6 ) { return redirect('/simulacro'); }
-        if( auth()->user()->id_rol == 1 ) { return redirect('/admin'); } 
+        if( auth()->user()->id_rol == 1 ) { return redirect('/admin/dashboard'); } 
         if( auth()->user()->id_rol == 2 ) { return redirect('/revisor'); }        
 
         return redirect()->intended(RouteServiceProvider::HOME);
