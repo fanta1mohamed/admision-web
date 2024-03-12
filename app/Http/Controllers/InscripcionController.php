a39963006b17bd884623863d197c865d9f81a20c
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2024-03-12 10:09:34 -0500

    update para mas de mil

--- app/Http/Controllers/InscripcionController.php
+++ app/Http/Controllers/InscripcionController.php
@@ -173,7 +173,7 @@ class InscripcionController extends Controller
         $prog = $request['postulante']['cod_programa'];
 
         $res = $siguiente = Inscripcion::where('codigo', 'like', 'G124'.$prog.'%')
-        ->max(\DB::raw('CAST(SUBSTRING(codigo, 8) AS UNSIGNED)')) + 1;
+        ->max(\DB::raw('CAST(SUBSTRING(codigo, 7) AS UNSIGNED)')) + 1;
         $res = str_pad($res, 4, '0', STR_PAD_LEFT);
 
         $inscripcion = Inscripcion::create([
