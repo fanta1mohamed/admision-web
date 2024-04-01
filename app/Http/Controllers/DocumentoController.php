44d8b526400b76d46c42ed2662e49d11d8b1e571
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2024-04-01 10:16:13 -0500

    -

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -124,7 +124,7 @@ class DocumentoController extends Controller {
 
   public function getCodigoDNI($dni){
 
-    $fechas = ['2024-03-25', '2024-03-26', '2024-03-27', '2024-03-28'];
+    $fechas = ['2024-03-25', '2024-03-26', '2024-03-27', '2024-03-28','2024-04-01'];
 
     $res = Documento::select('documento.codigo as label', 'documento.codigo as value')
         ->join('postulante', 'postulante.id', '=', 'documento.id_postulante')
