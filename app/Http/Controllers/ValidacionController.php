db0648274667046c732bd51d72f89262d642f987
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2024-07-14 22:37:05 -0500

    -

--- app/Http/Controllers/ValidacionController.php
+++ app/Http/Controllers/ValidacionController.php
@@ -8,17 +8,17 @@ use App\Models\Postulante;
 class ValidacionController extends Controller {
 
     public function existeCelular(Request $request) {
-        $postulante = Postulante::where('celular', $request->celular)->first();
-        if (!$postulante) {return response()->json(false, 200); }   
-        $nro_doc_correcto = $postulante->dni == $request->dni;
-        return response()->json($nro_doc_correcto, 200);
+        $postulante = Postulante::select('celular','nro_doc')->where('celular', $request->cel)->first();
+        if (!$postulante) { return response()->json(false, 200); }        
+        $nro_doc_correcto = $postulante->nro_doc == $request->dni;
+        return response()->json(!$nro_doc_correcto, 200);
     }
 
     public function existeCorreo(Request $request) {
         $postulante = Postulante::where('email', $request->email)->first();
         if (!$postulante) {return response()->json(false, 200); }   
-        $nro_doc_correcto = $postulante->dni == $request->dni;
-        return response()->json($nro_doc_correcto, 200);
+        $nro_doc_correcto = $postulante->nro_doc == $request->dni;
+        return response()->json(!$nro_doc_correcto, 200);
     }
 
     
