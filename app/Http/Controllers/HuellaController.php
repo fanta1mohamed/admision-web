837f4bc9cf7d5cb3a959ddd10e28ca570ae35cd4
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2024-08-12 19:12:05 -0500

    Update HuellaController.php

--- app/Http/Controllers/HuellaController.php
+++ app/Http/Controllers/HuellaController.php
@@ -100,7 +100,39 @@ class HuellaController extends Controller
             return response()->json(['error' => 'Error al cargar la imagen: ' . $e->getMessage()], 500);
         }
     }
+
+
+    public function uploadJuli(Request $request) {
+        try {
+            $dni = $request->input('dni');
+            $etapa = $request->input('etapa');
+            if ($request->hasFile('imagen')) {
+                $imagen = $request->file('imagen');
+
+                $rutaCarpeta = "";
+                if($etapa == 'inscripcion'){ 
+                     $rutaCarpeta = public_path('documentos/11/inscripciones/huellas/'); 
+                } else {  $rutaCarpeta = public_path('documentos/11/control_biometrico/huellas/'); 
+                }
+
+                if (!file_exists($rutaCarpeta)) {
+                    if (!mkdir($rutaCarpeta, 0777, true)) {
+                        return response()->json(['error' => 'No se pudo crear la carpeta para guardar la imagen'], 500);
+                    }
+                }
+
+                $imageName = $dni;
+                $imagen->move($rutaCarpeta, $imageName);
     
+                return response()->json(['image_path' => $dni]);
+            } else {
+                return response()->json(['error' => 'No se proporcionó ningún archivo de imagen'], 400);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error al cargar la imagen: ' . $e->getMessage()], 500);
+        }
+    }
+
 
 
 
