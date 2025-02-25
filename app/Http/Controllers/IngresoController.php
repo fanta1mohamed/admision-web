a38e16d6cb17ebb59d5ee46d9dd390aed8cf2651
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2025-02-25 03:49:27 -0500

    Update IngresoController.php

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -181,97 +181,106 @@ class IngresoController extends Controller {
         // return response()->download($outputFilePath)->deleteFileAfterSend();
     }
 
-    public function registrar_biometrico($dni){
-
-        $re = DB::select("SELECT
-        procesos.anio, procesos.ciclo_oti,
-        programa.programa_oti,
-        postulante.primer_apellido AS paterno,
-        postulante.segundo_apellido AS materno, postulante.nombres,
-        tipo_documento_identidad.documento_oti AS tipo_doc_oti,
-        postulante.nro_doc AS dni,
-        users.name, users.paterno as upaterno,
-        postulante.fec_nacimiento,
-        postulante.sexo,
-        postulante.ubigeo_residencia,
-        postulante.direccion,
-        postulante.estado_civil,
-        resultados.fecha,
-        postulante.email,
-        postulante.celular,
-        programa.cod_esp,
-        modalidad.modalidad_oti,
-        resultados.puntaje,
-        resultados.puesto,
-        resultados.puesto_general,
-        postulante.id AS id_postulante,
-        procesos.id AS id_proceso, procesos.nombre AS proceso,
-        modalidad.id AS id_modalidad, modalidad.nombre AS modalidad,
-        programa.nombre AS programa, programa.id,
-        control_biometrico.codigo_ingreso as codigo
-        FROM resultados
-        JOIN postulante ON resultados.dni_postulante =  postulante.nro_doc
-        JOIN inscripciones ON inscripciones.id_postulante = postulante.id
-        JOIN modalidad ON inscripciones.id_modalidad = modalidad.id
-        JOIN procesos ON resultados.id_proceso = procesos.id
-        LEFT join users on users.id = inscripciones.id_usuario
-        JOIN programa ON programa.id = inscripciones.id_programa
-        JOIN tipo_documento_identidad ON postulante.tipo_doc = tipo_documento_identidad.id
-        JOIN control_biometrico ON control_biometrico.id_postulante = postulante.id
-        AND control_biometrico.id_proceso = ".auth()->user()->id_proceso ." WHERE resultados.apto = 'SI' AND inscripciones.estado = 0
-        AND resultados.dni_postulante = $dni AND resultados.id_proceso = " . auth()->user()->id_proceso ." AND inscripciones.id_proceso =  ".auth()->user()->id_proceso);
-
+    public function registrar_biometrico($dni)
+    {
         try {
-            DB::transaction(function () use ($re) {
 
-                $ingreso = 1;
-                $i_admision = 0;
-                $e_civil = 1;
-
-                if($re[0]->estado_civil == 1 ) { $e_civil = 2;}
-                if($re[0]->estado_civil == 2 ) { $e_civil = 1;}
-                if($re[0]->estado_civil == 3 ) { $e_civil = 3;}
-                if($re[0]->estado_civil == 4 ) { $e_civil = 6;}
-
+            $postulante = Postulante::select([
+                'procesos.anio', 'procesos.ciclo_oti',
+                'programa.programa_oti',
+                'postulante.primer_apellido AS paterno',
+                'postulante.segundo_apellido AS materno', 'postulante.nombres',
+                'tipo_documento_identidad.documento_oti AS tipo_doc_oti',
+                'postulante.nro_doc AS dni',
+                'users.name', 'users.paterno as upaterno',
+                'postulante.fec_nacimiento',
+                'postulante.sexo',
+                'postulante.ubigeo_residencia',
+                'postulante.direccion',
+                'postulante.estado_civil',
+                'resultados.fecha',
+                'postulante.email',
+                'postulante.celular',
+                'programa.cod_esp',
+                'modalidad.modalidad_oti',
+                'resultados.puntaje',
+                'resultados.puesto',
+                'resultados.puesto_general',
+                'postulante.id AS id_postulante',
+                'procesos.id AS id_proceso', 'procesos.nombre AS proceso',
+                'modalidad.id AS id_modalidad', 'modalidad.nombre AS modalidad',
+                'programa.nombre AS programa', 'programa.id',
+                'control_biometrico.codigo_ingreso as codigo'
+            ])
+            ->join('resultados', 'resultados.dni_postulante', '=', 'postulante.nro_doc')
+            ->join('inscripciones', 'inscripciones.id_postulante', '=', 'postulante.id')
+            ->join('modalidad', 'inscripciones.id_modalidad', '=', 'modalidad.id')
+            ->join('procesos', 'resultados.id_proceso', '=', 'procesos.id')
+            ->leftJoin('users', 'users.id', '=', 'inscripciones.id_usuario')
+            ->join('programa', 'programa.id', '=', 'inscripciones.id_programa')
+            ->join('tipo_documento_identidad', 'postulante.tipo_doc', '=', 'tipo_documento_identidad.id')
+            ->join('control_biometrico', function($join) {
+                $join->on('control_biometrico.id_postulante', '=', 'postulante.id')
+                     ->on('control_biometrico.id_proceso', '=', DB::raw(auth()->user()->id_proceso));
+            })
+            ->where([
+                ['resultados.apto', '=', 'SI'],
+                ['inscripciones.estado', '=', 0],
+                ['resultados.dni_postulante', '=', $dni],
+                ['resultados.id_proceso', '=', auth()->user()->id_proceso],
+                ['inscripciones.id_proceso', '=', auth()->user()->id_proceso]
+            ])
+            ->first();
+    
+            // Si no se encuentra el postulante, retornar error
+            if (!$postulante) {
+                return response()->json(['error' => 'No se encontró el postulante o no cumple los requisitos.'], 404);
+            }
+    
+            DB::transaction(function () use ($postulante) {
+                $estadoCivilMap = [
+                    1 => 2,
+                    2 => 1,
+                    3 => 3,
+                    4 => 6
+                ];
+    
                 $estudiante = Estudiante::on('mysql_secondary')->create([
-                    'num_mat' => $re[0]->codigo,
-                    'cod_car' => $re[0]->programa_oti,
-                    'paterno' => $re[0]->paterno,
-                    'materno' => $re[0]->materno,
-                    'nombres' => $re[0]->nombres,
-                    'tip_doc' => $re[0]->tipo_doc_oti,
-                    'num_doc' => $re[0]->dni,
-                    'num_car' => $ingreso,
-                    'fch_nac' => $re[0]->fec_nacimiento,
-                    'sexo' => $re[0]->sexo,
-                    'ubigeo' => $re[0]->ubigeo_residencia,
-                    'mod_ing' => $re[0]->modalidad_oti,
-                    'est_civ' => $e_civil,
-                    'fch_ing' => $re[0]->fecha,
-                    'direc' => $re[0]->direccion,
-                    'email' => $re[0]->email,
+                    'num_mat' => $postulante->codigo,
+                    'cod_car' => $postulante->programa_oti,
+                    'paterno' => $postulante->paterno,
+                    'materno' => $postulante->materno,
+                    'nombres' => $postulante->nombres,
+                    'tip_doc' => $postulante->tipo_doc_oti,
+                    'num_doc' => $postulante->dni,
+                    'num_car' => 1, // Ingreso
+                    'fch_nac' => $postulante->fec_nacimiento,
+                    'sexo' => $postulante->sexo,
+                    'ubigeo' => $postulante->ubigeo_residencia,
+                    'mod_ing' => $postulante->modalidad_oti,
+                    'est_civ' => $estadoCivilMap[$postulante->estado_civil] ?? $postulante->estado_civil,
+                    'fch_ing' => $postulante->fecha,
+                    'direc' => $postulante->direccion,
+                    'email' => $postulante->email,
                     'con_est' => 5,
-                    'celular' => $re[0]->celular,
-                    'cod_esp' => $re[0]->cod_esp,
-                    'puntaje' => $re[0]->puntaje,
-                    'puesto_escuela' => $re[0]->puesto,
-                    'puesto_general' => $re[0]->puesto_general,
-                    'ano_ing' => $re[0]->anio,
-                    'per_ing' => $re[0]->ciclo_oti
-
+                    'celular' => $postulante->celular,
+                    'cod_esp' => $postulante->cod_esp,
+                    'puntaje' => $postulante->puntaje,
+                    'puesto_escuela' => $postulante->puesto,
+                    'puesto_general' => $postulante->puesto_general,
+                    'ano_ing' => $postulante->anio,
+                    'per_ing' => $postulante->ciclo_oti
                 ]);
-
             });
+    
+            return response()->json(['mensaje' => 'Registro biométrico realizado con éxito'], 200);
+    
         } catch (\Exception $e) {
-            $errorMessage = $e->getMessage();
-            \Log::error('Error en la transacción: ' . $errorMessage);
-
-            return response()->json(['error' => 'Ocurrió un error en la transacción: ' . $errorMessage], 500);
+            \Log::error('Error en la transacción: ' . $e->getMessage());
+            return response()->json(['error' => 'Ocurrió un error en la transacción: ' . $e->getMessage()], 500);
         }
-
-        $this->response['estado'] = true;
-        return response()->json($this->response, 200);
     }
+    
 
 
 
