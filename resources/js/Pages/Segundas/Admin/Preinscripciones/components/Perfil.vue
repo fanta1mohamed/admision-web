df4030390cd3b09a1aa933205bbf0f3578709dad
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2025-03-12 11:51:21 -0500

    corrección de cache

--- resources/js/Pages/Segundas/Admin/Preinscripciones/components/Perfil.vue
+++ resources/js/Pages/Segundas/Admin/Preinscripciones/components/Perfil.vue
@@ -1,37 +1,35 @@
 <template>
-    <div class="flex  overflow-hidden shadow-sm sm:rounded-lg  justify-center">
+    <div class="flex overflow-hidden shadow-sm sm:rounded-lg justify-center">
         <div class="bg-white p-5 container">
             <div class="p-2">
                 <a-row class="p-4" :gutter="[16, 8]" style="border: solid #d9d9d9 1px; border-radius: 9px;">
                     <a-col :xs="24" :sm="12" :md="8" :lg="6">
                         <div style="height: 100%; overflow:hidden;">
                             <div style="margin-bottom: 0px; margin-top:0px">
-                                    <img :src="foto" :preview="false" />
+                                <img :src="foto" :preview="false" />
                             </div>
                         </div>
                     </a-col>
                     <a-col :xs="24" :sm="12" :md="16" :lg="18">
                         <div class="flex justify-between" v-if="info">
-                            <div class="ml-4" >
+                            <div class="ml-4">
                                 <div class="flex mt-5">
                                     <span style="font-weight: bold; font-size: 2.0rem;">{{ info.primer_apellido }} {{ info.segundo_apellido }} {{ info.nombres }}</span>
                                 </div>
                                 <div style="margin-top:0px;">
-                                    <span style="font-size:1.1rem; color:gray; ">{{ info.email }}</span>
+                                    <span style="font-size:1.1rem; color:gray;">{{ info.email }}</span>
                                 </div>
                                 <div style="margin-top:5px;">
-                                    <USerOutlined />
-                                    <span style="font-size:1.0rem; font-weight:light; color:gray; ">{{ info.celular }}</span>
+                                    <UserOutlined />
+                                    <span style="font-size:1.0rem; font-weight:light; color:gray;">{{ info.celular }}</span>
                                 </div>
                                 <div style="margin-top:5px;">
-                                    <USerOutlined />
+                                    <UserOutlined />
                                 </div>
-
                                 <div style="margin-top:5px;">
-                                    <USerOutlined />
-                                    <span style="font-size:1.0rem; font-weight:light; color:gray; ">{{ info.departamento }} - {{ info.provincia }} - {{ info.distrito }}</span>
+                                    <UserOutlined />
+                                    <span style="font-size:1.0rem; font-weight:light; color:gray;">{{ info.departamento }} - {{ info.provincia }} - {{ info.distrito }}</span>
                                 </div>
-
                                 <div class="flex" style="margin-top:5px;">
                                     <div class="mr-3" style="display:flex; align-items:center;">
                                         <FormOutlined /> {{ preinscripciones }}
@@ -43,10 +41,9 @@
                                         <UserOutlined /> {{ control_biometrico }}
                                     </div>
                                     <div class="mr-3" style="display:flex; align-items:center;">
-                                        <DownloadOutlined /> 
+                                        <DownloadOutlined />
                                     </div>
                                 </div>
-
                             </div>
                             <div class="mt-6">
                                 <a-button type="primary" @click="abrir()" disabled>
@@ -55,84 +52,76 @@
                                         <DownloadOutlined />
                                     </template>
                                 </a-button>
-
                             </div>
                         </div>
-                        <div>
-
+                    </a-col>
+                </a-row>
+            </div>
+            <div>
+                <a-row>
+                    <a-col :xs="24" :sm="24" :md="24" :lg="24">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5 mt-4"
+                            style="border: solid #d9d9d9 1px; min-height: 600px;">
+                            <iframe :src="titulo" frameborder="0" width="100%" style="min-height: 600px;"></iframe>
                         </div>
                     </a-col>
                 </a-row>
             </div>
-
-            <div>
-                <a-row>
-                <a-col :xs="24" :sm="24" :md="24" :lg="24">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5 mt-4"
-                style="border: solid #d9d9d9 1px; min-height: 600px;">
-                <iframe :src="titulo" frameborder="0" width="100%" style="min-height: 600px;" ></iframe>
-                </div>
-                    </a-col>
-                </a-row>
-            </div>
-
-            </div>
-
+        </div>
     </div>
 
-    <a-modal v-model:open="open" title="Editar Datos">
-
-    </a-modal>
-
-
-    <a-modal v-model:open="open" title="Modal">
-        sdfasdf
-    </a-modal>
+    <a-modal v-model:open="open" title="Editar Datos"></a-modal>
+    <a-modal v-model:open="open" title="Modal">sdfasdf</a-modal>
 </template>
 
 <script setup>
-import { reactive, ref, onMounted, watch, defineProps } from 'vue';
-import { FormOutlined, DeleteOutlined, SearchOutlined, DownloadOutlined, UserOutlined, RightOutlined, BookOutlined } from '@ant-design/icons-vue';
-import { notification } from 'ant-design-vue';
-import dayjs from 'dayjs';
-import { format } from 'date-fns';
+import { ref, onMounted, watch } from 'vue';
+import { FormOutlined, DeleteOutlined, DownloadOutlined, UserOutlined } from '@ant-design/icons-vue';
 import axios from 'axios';
+
 const props = defineProps(['dni']);
 
 const info = ref(null);
-
 const foto = ref("");
 const titulo = ref("");
 const infoColegio = ref("");
 const preinscripciones = ref("");
 const inscripciones = ref("");
 const control_biometrico = ref("");
-
-
 const open = ref(false);
 
 const abrir = () => {
     open.value = true;
     console.log("Esta funcionando");
-}
+};
 
-const getPostulante =  async ( ) => {
-    let res = await axios.get( "/segundas/get-postulante-datos/"+props.dni);
-    info.value = res.data.info;
-    titulo.value = res.data.titulo;
-    infoColegio.value = res.data.infoColegio;
-    preinscripciones.value = res.data.preinscripciones;
-    inscripciones.value = res.data.inscripciones;
-    control_biometrico.value = res.data.control_biometrico;
-}
+const getPostulante = async () => {
+    try {
+        let res = await axios.get("/segundas/get-postulante-datos/" + props.dni);
+        info.value = res.data.info;
+        titulo.value = res.data.titulo;
+        infoColegio.value = res.data.infoColegio;
+        preinscripciones.value = res.data.preinscripciones;
+        inscripciones.value = res.data.inscripciones;
+        control_biometrico.value = res.data.control_biometrico;
+    } catch (error) {
+        console.error("Error al obtener los datos del postulante:", error);
+    }
+};
 
-getPostulante();
+// Ejecutar getPostulante al montar el componente
+onMounted(() => {
+    getPostulante();
+});
 
-
-
-const seleccionado = ref(null);
-
+// Observar cambios en props.dni y volver a ejecutar getPostulante
+watch(() => props.dni, (newDni) => {
+    if (newDni) {
+        getPostulante();
+    }
+});
 </script>
+
 <style scoped>
 .item-inscripciones {
     cursor: pointer;
@@ -166,10 +155,5 @@ const seleccionado = ref(null);
 
 .container {
     max-width: 1200px;
-}</style>
-
-
-
-
-
-
+}
+</style>
