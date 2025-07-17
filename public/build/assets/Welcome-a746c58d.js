23b672cebf2430fb7526c5a78ad37d8bbc2065b5
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2025-07-17 16:31:46 -0500

    ratio _ build

---
+++ public/build/assets/Welcome-a746c58d.js
@@ -0,0 +1,1 @@
+import{o as e,E as r,u as t,Z as o}from"./app-4942be61.js";const c={__name:"Welcome",props:{canLogin:{type:Boolean},canRegister:{type:Boolean},laravelVersion:{type:String,required:!0},phpVersion:{type:String,required:!0}},setup(n){return(a,p)=>(e(),r(t(o),{title:"Welcome"}))}};export{c as default};
