da1bd46face86cc2dc47c68d01089f06d7ffb0a3
ariellukezz <40122258+ariellukezz@users.noreply.github.com>
2025-07-17 07:40:22 -0500

    Update DescargarArchivosController.php

--- app/Http/Controllers/DescargarArchivosController.php
+++ app/Http/Controllers/DescargarArchivosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use ZipStream\ZipStream;
 use ZipArchive;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\File;
@@ -12,31 +13,28 @@ use Illuminate\Support\Facades\File;
 class DescargarArchivosController extends Controller
 {
 
-    public function downloadZip()
+  public function downloadZip()
 {
     $folder = public_path('documentos/' . auth()->user()->id_proceso);
     $filename = 'documentos_' . auth()->user()->id_proceso . '.zip';
 
-    $response = new StreamedResponse(function () use ($folder) {
-        $zip = new \ZipStream\ZipStream(null, [
-            'outputName' => 'archivos.zip',
-            'sendHttpHeaders' => false,
-        ]);
+    return new StreamedResponse(function () use ($folder) {
+        $options = new \ZipStream\Option\Archive();
+        $options->setSendHttpHeaders(true);
 
+        $zip = new ZipStream(null, $options);
         $files = File::allFiles($folder);
 
         foreach ($files as $file) {
-            $relativeName = str_replace($folder . '/', '', $file->getRealPath());
-            $zip->addFileFromPath($relativeName, $file->getRealPath());
+            $relativePath = ltrim(str_replace($folder, '', $file->getPathname()), '/\\');
+            $zip->addFileFromPath($relativePath, $file->getRealPath());
         }
 
         $zip->finish();
-    });
-
-    $response->headers->set('Content-Type', 'application/octet-stream');
-    $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '"');
-
-    return $response;
+    }, 200, [
+        'Content-Type' => 'application/octet-stream',
+        'Content-Disposition' => 'attachment; filename="' . $filename . '"',
+    ]);
 }
 
 
